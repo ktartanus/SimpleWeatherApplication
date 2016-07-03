9c340f4ee2eebfc497d70833887f351793243670
ktartanus <kamiltartanus@gmail.com>
2016-07-03 21:47:47 +0200

    copy all components

---
+++ weather.js
@@ -0,0 +1,82 @@
+﻿"use strict";
+
+function WeatherApp()
+{
+    var weatherWidget = new WeatherWidget($("#weather-widget"), "a283a864fe26fd6b"),
+        version = "8.3";
+        
+    function getLocation()
+    {
+        if (navigator.geolocation)
+        {
+            // Use geolocation to get current position
+            navigator.geolocation.getCurrentPosition(function(position)
+            {
+                $("#latitude").val(position.coords.latitude);
+                $("#longitude").val(position.coords.longitude);
+            },
+            function(error)
+            {
+                $("#controls .error")
+                    .text("ERROR: " + error.message)
+                    .slideDown();
+            });
+        }
+    }
+    function changedLocation()
+    {
+	if(navigator.geolocation)
+	{
+		navigator.geolocation.watchPosition(function(position)
+            {
+		var latitude = position.coords.latitude;
+		var longtitude = position.coords.longitude;              
+		$("#latitude").val(latitude);
+                $("#longitude").val(longtitude);
+		$("#mapImage").attr("src", mapImageReader(latitude,longtitude));
+            },
+            function(error)
+            {
+                $("#controls .error")
+                    .text("ERROR: " + error.message)
+                    .slideDown();
+            });
+	}
+    }
+
+    function mapImageReader(posX, posY, zoom, size)
+    {
+	var zoom = zoom || 15;
+	var size = size || "600x300";
+	if (posX && posY)
+	{
+	    var src = "https://maps.googleapis.com/maps/api/staticmap?center=" + posX + "," + posY + "&markers=color:blue%7Clabel:I%7C" + posX + "," + posY + "&zoom=" + zoom + "&size=" + size + "&maptype=roadmap&key=AIzaSyDEuWqwGsiNTtiDyG87lHN5jE5OdMKwhKk";
+	    return src;	
+	}
+    }
+
+    function getCurrentWeather()
+    {
+        var lat = $("#latitude").val();
+        var lon = $("#longitude").val();
+        if (lat && lon)
+        {
+            $("#weather-widget").fadeIn();
+            weatherWidget.update(lat, lon);
+        }
+    }
+    
+    this.start = function()
+    {
+        $("#app>header").append(version);
+        $("#getWeather").click(getCurrentWeather);
+        getLocation();
+	changedLocation();
+    }
+}
+
+$(function()
+{
+    window.app = new WeatherApp();
+    window.app.start();
+});
---
+++ weatherWidget.js
@@ -0,0 +1,54 @@
+﻿function WeatherWidget($widget, wuKey)
+{
+    "use strict";
+
+    this.update = function(lat, lon)
+    {
+        $(".results", $widget).hide();
+        $(".loading", $widget).show();
+        getWeatherReport(lat, lon);
+    };
+
+    function getWeatherReport(lat, lon)
+    {
+        var coords = lat + "," + lon;
+        $.ajax({
+            url: "http://api.wunderground.com/api/" + wuKey + "/conditions/q/" + coords + ".json", 
+            dataType : "jsonp"
+        })
+        .done(function(data) { populateWeather(data); })
+        .fail(function(jqXHR, textStatus, errorThrown) { showError(errorThrown); });
+    }
+    
+    function populateWeather(data)
+    {
+        var observation = data.current_observation;
+        
+        $(".results header img", $widget).attr("src", observation.icon_url);
+        $(".location>span", $widget).text(observation.display_location.full);
+        
+        $(".conditions>span").each(function(i, e)
+        {
+            var $span = $(this);
+            var field = $span.data("field");
+            $(this).text(observation[field]);
+        });
+        
+        // Comply with terms of service
+        $(".results footer img", $widget).attr("src", observation.image.url);
+        
+        $(".loading", $widget).fadeOut(function ()
+        {
+            $(".results", $widget).fadeIn();
+        });
+    }
+    
+    function showError(msg)
+    {
+        $(".error>span", $widget).text(msg);
+        $(".loading", $widget).fadeOut(function ()
+        {
+            $(".error", $widget).fadeIn();
+        });
+    }
+}
